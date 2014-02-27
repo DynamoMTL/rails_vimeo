48d15679a4b22af6012e0730c851ca6d99dc754d
Nicolas Charlery <nicolas.charlery@gmail.com>
2014-02-27 10:13:19 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CineflixVimeo::Application.config.secret_key_base = 'e7a4e3259101746271a6c7b13c78e36208a46945832e2a062b845211681cd6613fddd17f3465b6327b1a273807ee277ca6d4c08d22d3bbd57899d5d004f27218'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CineflixVimeo::Application.config.session_store :cookie_store, key: '_cineflix_vimeo_session'
