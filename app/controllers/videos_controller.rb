eeb7535d93fe406503558367c17d9009d8b9ffab
Nicolas Charlery <nicolas.charlery@gmail.com>
2014-02-27 11:10:46 +0100

    Added an index page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,13 @@
 class VideosController < ApplicationController
+  def index
+  end
+
   def new
   end
 
   def create
     title = params[:title]
     video_io = params[:video_file]
+    redirect_to videos_path
   end
 end
